df285b771b9c05677c245e377ddffb09367d8d77
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-03-19 01:30:11 -0300

    Controllers e Query String

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,19 @@ const pagina1 = (req, res) => {
   res.send('Fullstack Master')
 }
 
+const calc = (req, res) => {
+  // console.log(req.query)
+  if (req.query.num1 && req.query.num2) {
+    const { num1, num2 } = req.query
+    const soma = parseFloat(num1) + parseFloat(num2)
+    res.send('A soma é: ' + soma)
+  } else {
+    res.send('Calculadora')
+  }
+}
+
 module.exports = {
   home,
-  pagina1
+  pagina1,
+  calc
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const indexController = require('../controllers/index')
 
 router.get('/', indexController.home)
 router.get('/pagina1', indexController.pagina1)
+router.get('/calc', indexController.calc)
 
 module.exports = router
